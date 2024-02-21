b123422196d8c26b12e5320769addeddf6897839
PetsaYuri <petsa.yuri@gmail.com>
2024-02-21 19:35:13 +0200

    init

---
+++ src/main/java/com/online/students/service/OnlineStudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.online.students.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStudentsServiceApplication.class, args);
+	}
+
+}
